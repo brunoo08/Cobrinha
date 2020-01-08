79718eaaf671422d571cdccc97797950509f7015
Bruo <bruno_felipelustosa@hotmail.com>
2020-01-08 12:48:49 -0300

    cobrinha incompleta 2

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 var cobra = [];
 var posX = 105;
 var posY = 105;
-var grid = 21;
-var andaX = 0;
-var andaY = 0;
 var macaX = 0;
 var macaY = 0;
 var clicado = true;
 var direcao = 0;
-var x = 37;
 var p = 1;
+var i = 0;
+var pegaTop = 0;
+var pegaLeft = 0;
+var cabeca = $("<div></div>", {
+    id: "ponto" + i,
+    class: "ponto",
+    style: "top:105px;left:105px"
+})
+var macaInicio = $("<div></div>", {
+    id: "maca"
+})
 
 $(document).ready(function () {
     $("#principal").click(function () {
@@ -22,68 +29,12 @@ $(document).ready(function () {
 })
 
 function jogo() {
-    var c = document.getElementById("principal");
-    var d = document.createElement("div");
-    cobra[0] = $("#principal").append("<div id='ponto'></div>");
-    cobra.length ++;
-    $("#principal").append("<div id='maca'></div>");
+    cobra[i] = $("#principal").append(cabeca);
+    cobra.length + 1;
+    console.log(i);
+    $("#principal").append(macaInicio);
     maca();
     anda();
-}
-
-$(document).keydown(function (e) {
-    if ((e.keyCode == 37) || (e.keyCode == 38) || (e.keyCode == 39) || (e.keyCode == 40))
-        direcao = e.keyCode;
-});
-
-function anda() {
-    var tmp = setInterval(move, 150);
-    function move() {
-        switch (direcao) {
-            case 37:
-                direcao = 37;
-                if (posX < 1) {
-                    $("#perdeu").css("opacity", 1);
-                } else {
-                    $("#ponto").css("left", (posX - 7) + "px");
-                    posX = posX - 7;
-                    pega();
-                }
-                break;
-            case 38:
-                direcao = 38;
-                if (posY < 1) {
-                    $("#perdeu").css("opacity", 1);
-                } else {
-                    $("#ponto").css("top", (posY - 7) + "px");
-                    posY = posY - 7;
-                    pega();
-                }
-                break;
-            case 39:
-                direcao = 39;
-                if (posX > 201) {
-                    $("#perdeu").css("opacity", 1);
-                } else {
-                    $("#ponto").css("left", (posX + 7) + "px");
-                    posX = posX + 7;
-                    pega();
-                }
-                break;
-            case 40:
-                direcao = 40;
-                if (posY > 201) {
-                    $("#perdeu").css("opacity", 1);
-
-                } else {
-                    $("#ponto").css("top", (posY + 7) + "px");
-                    posY = posY + 7;
-                    pega();
-                }
-                break;
-        }
-    }
-    pega();
 }
 
 function maca() {
@@ -100,6 +51,60 @@ function maca() {
     $("#maca").css("top", (macaY) + "px");
 }
 
+$(document).keydown(function (e) {
+    if ((e.keyCode == 37) || (e.keyCode == 38) || (e.keyCode == 39) || (e.keyCode == 40))
+        direcao = e.keyCode;
+});
+
+function anda() {
+    var tmp = setInterval(move, 150);
+    function move() {
+        switch (direcao) {
+            case 37:
+                direcao = 37;
+                if (posX < 1) {
+                    $("#perdeu").css("opacity", 1);
+                } else {
+                    $("#ponto" + i).css("left", (posX - 7) + "px");
+                    posX = posX - 7;
+                    pega();
+                }
+                break;
+            case 38:
+                direcao = 38;
+                if (posY < 1) {
+                    $("#perdeu").css("opacity", 1);
+                } else {
+                    $("#ponto" + i).css("top", (posY - 7) + "px");
+                    posY = posY - 7;
+                    pega();
+                }
+                break;
+            case 39:
+                direcao = 39;
+                if (posX > 201) {
+                    $("#perdeu").css("opacity", 1);
+                } else {
+                    $("#ponto" + i).css("left", (posX + 7) + "px");
+                    posX = posX + 7;
+                    pega();
+                }
+                break;
+            case 40:
+                direcao = 40;
+                if (posY > 201) {
+                    $("#perdeu").css("opacity", 1);
+
+                } else {
+                    $("#ponto" + i).css("top", (posY + 7) + "px");
+                    posY = posY + 7;
+                    pega();
+                }
+                break;
+        }
+    }
+}
+
 function pega() {
     if (((macaY + 7) == posY) && (macaX == posX)) {
         document.getElementById("pontos").innerHTML = "Pontos: " + p;
@@ -109,8 +114,37 @@ function pega() {
     }
 }
 
-function cresce(){
-    for(var i = 1; i < cobra.length; i ++){
-        cobra[i] = $("#ponto").append("<div id='ponto'></div>"); 
+function cresce() {
+    console.log(cobra);
+    // pegaTop = $("#ponto" + (cobra.length - 1)).position().top;
+    pegaTop = posY;
+    console.log(pegaTop);
+    // pegaLeft = $("#ponto" + (cobra.length - 1)).position().left;
+    pegaLeft = posX;
+    console.log(pegaLeft);
+    if (direcao == 37) {
+        $("#principal").append(cabeca);
+        console.log(i);
+        cobra[i] = $("#ponto" + i).css("top", (pegaTop) + "px");
+        cobra[i] = $("#ponto" + i).css("left", (pegaLeft + 7) + "px");
     }
+    if (direcao == 38) {
+        $("#principal").append(cabeca);
+        console.log(i);
+        cobra[i] = $("#ponto" + i).css("top", (pegaTop + 7) + "px");
+        cobra[i] = $("#ponto" + i).css("left", (pegaLeft) + "px");
+    }
+    if (direcao == 39) {
+        $("#principal").append(cabeca);
+        console.log(i);
+        cobra[i] = $("#ponto" + i).css("top", (pegaTop) + "px");
+        cobra[i] = $("#ponto" + i).css("left", (pegaLeft - 7) + "px");
+    }
+    if (direcao == 40) {
+        $("#principal").append(cabeca);
+        console.log(i);
+        cobra[i] = $("#ponto" + i).css("top", (pegaTop - 7) + "px");
+        cobra[i] = $("#ponto" + i).css("left", (pegaLeft) + "px");
+    }
+    i++;
 }
